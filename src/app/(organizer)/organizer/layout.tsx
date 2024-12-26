80b42429ef748520092485196e14de8b2cc31fc0
hanifahmad <136031772+rizqiamad@users.noreply.github.com>
2024-12-26 21:02:15 +0700

    Update layout.tsx

--- src/app/(organizer)/organizer/layout.tsx
+++ src/app/(organizer)/organizer/layout.tsx
@@ -4,9 +4,8 @@ import MobileNav from "./mobileNav/page";
 
 export default function OrganizerLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="items-start justify-between
-    lg:flex md:flex">
-      {<Sidebar />}
+    <div className="items-start justify-between lg:flex md:flex">
+      <Sidebar />
       <MobileNav />
       <main className="w-full h-full relative">{children}</main>
     </div>
