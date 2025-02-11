ba20fbc30b3d11d4bc76a20d63682738f6730560
Hanif Ahmad <ahmadhanif759@gmail.com>
2025-02-11 10:22:06 +0700

    humberger menu

--- src/components/events/card.tsx
+++ src/components/events/card.tsx
@@ -29,7 +29,7 @@ export default function Card({
   const date = displayDate(formatDate(start_date), formatDate(end_date))
 
   return (
-    <Link target="blank" href={`events/${eventId}`} className="shadow-[0px_0px_5px_-1px_rgba(0,0,0,0.75)] min-w-[13rem] max-w-[17rem] h-[20rem] rounded-md overflow-hidden">
+    <Link target="blank" href={`events/${eventId}`} className="shadow-[0px_0px_5px_-1px_rgba(0,0,0,0.75)] min-w-[13rem] max-w-[13rem] h-[20rem] rounded-md overflow-hidden">
       <div className="relative w-full h-[40%]">
         <Image src={srcImgEvent} alt={eventTitle} fill />
       </div>
--- src/components/navbar/hamburgerMenu.tsx
+++ src/components/navbar/hamburgerMenu.tsx
@@ -3,6 +3,7 @@
 import UseClickOutside from "@/hooks/useClickOutside";
 import UseOpen from "@/hooks/useOpen";
 import useSession from "@/hooks/useSession";
+import { logOut } from "@/libs/action";
 import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -10,20 +11,26 @@ export default function HamburgerMenu() {
   const { open, hidden, menuHandler } = UseOpen()
   const { user } = useSession()
   UseClickOutside(open, menuHandler)
+
+  const onLogout = () => {
+    logOut("token");
+    location.reload();
+  };
+
   return (
     <>
       <button onClick={menuHandler}><GiHamburgerMenu className="text-xl" /></button>
-      <div className={`absolute top-14 right-10 w-64 py-4 bg-white text-black shadow-lg border font-semibold rounded-md flex-col gap-4 transition duration-300 ease-in-out flex ${hidden ? '' : 'hidden'} ${open ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}`}>
+      <div className={`absolute top-14 px-4 right-10 w-64 py-4 bg-white text-black shadow-lg border font-semibold rounded-md flex-col gap-4 transition duration-300 ease-in-out flex ${hidden ? '' : 'hidden'} ${open ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}`}>
         {user && (
           <div className="mb-2">
             <p className="font-semibold">{user.full_name}</p>
             <p className="text-xs text-gray-500 font-semibold">{user.email}</p>
           </div>
         )}
-        <Link href={'/member/bio'} className="px-4 hover:opacity-[0.8] flex items-center gap-1">Profile</Link>
-        <Link href={'/events'} className="px-4 hover:opacity-[0.8] flex items-center gap-2">Jelajahi</Link>
-        <Link href={'/member/ticket'} className="px-4 hover:opacity-[0.8] flex items-center gap-2">Tiket saya</Link>
-        <Link href={'/'} className="px-4 hover:opacity-[0.8] text-red-500 flex items-center gap-1">Logout</Link>
+        <Link href={'/member/bio'} className="hover:opacity-[0.8] flex items-center gap-1">Profile</Link>
+        <Link href={'/events'} className="hover:opacity-[0.8] flex items-center gap-2">Jelajahi</Link>
+        <Link href={'/member/ticket'} className="hover:opacity-[0.8] flex items-center gap-2">Tiket saya</Link>
+        <Link href={'#'} onClick={onLogout} className="hover:opacity-[0.8] text-red-500 flex items-center gap-1">Logout</Link>
       </div>
     </>
   )
